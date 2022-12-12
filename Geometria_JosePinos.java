e845d1a2c2983f696c8217e58a559861d5efd268
JosePinos <jose.pinos@epn.edu.ec>
2022-12-12 06:55:26 -0500

    Clase Geometria_JosePinos

---
+++ Geometria_JosePinos.java
@@ -0,0 +1,201 @@
+public class Geometria_JosePinos {
+
+    //ATRIBUTOS
+    private double area;
+    private double perimetro;
+    private double volumen;
+
+    //CONSTRUCTOR
+    public Geometria_JosePinos(){
+        area = 0.0;
+        perimetro = 0.0;
+        volumen = 0.0;
+    }
+
+
+    //MÉTODOS
+
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public double getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+
+
+    //triangulo
+    public void areaTriangulo(double base, double altura){
+        if(base > 0.0 && altura > 0.0){
+            area = base * altura / 2.0;
+        }else{
+            area = 0;
+        }
+
+    }
+
+    public void perimetroTriangulo(double l1, double l2, double l3){
+        if(l1 > 0.0 && l2 > 0.0 && l3 > 0.0){
+            perimetro = l1 + l2 + l3;
+        }else{
+            perimetro = 0;
+        }
+
+    }
+
+
+
+    //circulo y esfera
+    public void areaCirculo(double radio){
+        if(radio > 0.0){
+            area = Math.PI * radio * radio;
+        }else{
+            area = 0;
+        }
+
+    }
+
+    public void perimetroCirculo(double radio){
+
+        if(radio > 0.0){
+            perimetro = 2.0 * Math.PI * radio;
+        }else{
+            perimetro = 0;
+        }
+
+    }
+
+    public void areaEsfera(double radio){
+
+        if(radio > 0.0){
+            area = 4.0 * Math.PI * radio * radio;
+        }else{
+            area = 0;
+        }
+    }
+
+    public void volumenEsfera(double radio){
+
+        if(radio > 0.0){
+            volumen =  4/3.0 * Math.PI * radio * radio * radio;
+        }else{
+            volumen = 0;
+        }
+    }
+
+
+
+    //cuadrado y cubo
+    public void areaCuadrado(double lado){
+
+        area = lado > 0.0 ? lado * lado : 0.0;
+
+    }
+
+    public void perimetroCuadrado(double lado){
+        //perimetro = 4.0 * lado;
+        perimetro = lado > 0.0 ? 4.0 * lado : 0.0;
+    }
+
+
+    public void areaCubo(double lado){
+
+        area = lado > 0.0 ? 6.0 * lado * lado : 0.0;
+    }
+
+    public void volumenCubo(double lado){
+        volumen = lado > 0.0 ? lado * lado * lado : 0.0;
+    }
+
+
+
+    //RECTÁNGULO / PARALELEPÍPEDO / PARALELOGRAMO
+    public void areaRectangulo(double base, double altura){
+
+        if( base > 0.0 && altura > 0.0 ){
+            area = base*altura;
+        }else{
+            area = 0.0;
+        }
+    }
+
+    public void perimetroRectangulo(double base, double altura){
+
+        if( base > 0.0 && altura > 0.0 ){
+            perimetro = 2.0 * base  +  2.0 * altura;
+        }else{
+            perimetro = 0.0;
+        }
+    }
+
+    public void superficieParalelepipedo(double largo, double ancho, double alto){
+
+        if(largo > 0.0 && ancho > 0.0 && alto > 0.0){
+            double sup_lateral = 2.0 * largo * alto  +  2.0 * ancho * alto;
+            double sup_base = largo * ancho;
+            area = sup_lateral + (2.0 * sup_base);
+        }else{
+            area = 0.0;
+        }
+    }
+
+    public void volumenParalelepipedo(double largo, double ancho, double alto){
+
+        if( largo > 0.0 && ancho > 0.0 && alto > 0.0 ){
+            volumen = largo * ancho * alto;
+        }else{
+            volumen = 0.0;
+        }
+    }
+
+
+
+    //TRAPECIO
+    public void areaTrapecio(double baseMayor, double baseMenor, double altura){
+
+        if( baseMayor > 0.0 && baseMenor > 0.0 && altura > 0.0){
+            area = (baseMayor + baseMenor) * altura /2.0;
+        }else{
+            area = 0.0;
+        }
+    }
+
+
+
+    //CILINDRO RECTO
+    public void areaCilindro(double radio, double altura){
+
+        if( radio > 0.0 && altura > 0.0 ){
+            area = (2.0 * Math.PI * radio * altura) + (2.0 * Math.PI * radio* radio);
+        }else{
+            area = 0.0;
+        }
+    }
+
+    public void volumenCilindro(double radio, double altura){
+
+        if(radio > 0.0 && altura > 0.0){
+            volumen = (Math.PI * Math.pow(radio,2)) * altura;
+        }else{
+            volumen = 0.0;
+        }
+    }
+
+}///FIN DE LA CLASE GEOMETRIA_JOSEPINOS
---
+++ usoGeometria.java
@@ -0,0 +1,157 @@
+import javax.swing.JOptionPane;
+
+public class usoGeometria {
+
+    public static void main(String[] args){
+
+        Geometria_JosePinos Figura = new Geometria_JosePinos();
+        int opcion = 1;
+
+        while( opcion != 0){
+            opcion = Menu();
+
+            switch( opcion ) {
+
+                case 1:{
+                    Figura.areaTriangulo( Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base del tríangulo")) ,
+                         Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base del tríangulo"))
+                    );
+                    JOptionPane.showMessageDialog(null, "El área del triángulo es: " + Figura.getArea());
+                }
+                break;
+
+                case 2:{
+                    Figura.perimetroTriangulo( Double.parseDouble(JOptionPane.showInputDialog("Ingrese el lado1 del tríangulo")) ,
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese el lado 2 del tríangulo")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese el lado 3 del tríangulo"))
+                    );
+
+                    JOptionPane.showMessageDialog(null, "El perímetro del triángulo es: " + Figura.getPerimetro());
+                }
+                break;
+
+                case 3:{
+                    Figura.areaCirculo(Double.parseDouble(JOptionPane.showInputDialog("Ingresa el radio del círculo")));
+                    Figura.perimetroCirculo(Double.parseDouble(JOptionPane.showInputDialog("Ingresa el radio del círculo")));
+                    JOptionPane.showMessageDialog(null, "El área del círculo es: " + Figura.getArea() +
+                            "\n El perímetro del círculo es: " + Figura.getPerimetro());
+                }
+                break;
+
+                case 4:{
+                    Figura.areaEsfera(Double.parseDouble(JOptionPane.showInputDialog("Ingresa el radio de la esfera")));
+                    Figura.volumenEsfera(Double.parseDouble(JOptionPane.showInputDialog("Ingresa el radio de la esfera")));
+                    JOptionPane.showMessageDialog(null, "El área de la esfera es: " + Figura.getArea() +
+                            "\n El volumen de la esfera es: " + Figura.getVolumen());
+                }
+                break;
+
+                case 5:{
+                    Figura.areaCuadrado(Double.parseDouble(JOptionPane.showInputDialog("Ingresa el lado del cuadrado")));
+                    Figura.perimetroCuadrado(Double.parseDouble(JOptionPane.showInputDialog("Ingresa el lado del cuadrado")));
+                    JOptionPane.showMessageDialog(null,"EL área del cuadrado es: " + Figura.getArea() +
+                            "\nEl perímetro del cuadrado es: " + Figura.getPerimetro());
+                }
+                break;
+
+                case 6:{
+                    Figura.areaCubo(Double.parseDouble(JOptionPane.showInputDialog("Ingresa el lado del cuadrado")));
+                    Figura.volumenCubo(Double.parseDouble(JOptionPane.showInputDialog("Ingresa el lado del cuadrado")));
+                    JOptionPane.showMessageDialog(null, "El área del cubo es: " + Figura.getArea() +
+                            "\nEl volumen del cubo es: " + Figura.getVolumen());
+                }
+                break;
+
+                case 7:{
+                    Figura.areaRectangulo(Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base del rectángulo")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura del rectángulo")) );
+
+                    Figura.perimetroRectangulo(Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base del rectángulo")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura del rectángulo")) );
+
+                    JOptionPane.showMessageDialog(null, "El área del rectángulo es: " + Figura.getArea() +
+                            "\nEl perímetro del rectángulo es: " + Figura.getPerimetro());
+                }
+                break;
+
+                case 8:{
+                    Figura.superficieParalelepipedo( Double.parseDouble(JOptionPane.showInputDialog("Ingrese el largo del paralelepípedo")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese el ancho del paralelepípedo")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese el alto del paralelepípedo"))
+                            );
+
+                    Figura.volumenParalelepipedo( Double.parseDouble(JOptionPane.showInputDialog("Ingrese el largo del paralelepípedo")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese el ancho del paralelepípedo")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese el alto del paralelepípedo"))
+                    );
+
+                    JOptionPane.showMessageDialog(null, "El área del paralelepípedo es: " + Figura.getArea() +
+                            "\nEl volumen del paralelepípedo es: " + Figura.getVolumen());
+                }
+                break;
+
+                case 9:{
+                    Figura.areaTrapecio( Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base mayor del trapecio")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base menor del trapecio")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura del trapecio"))
+                    );
+
+                    JOptionPane.showMessageDialog(null, "El área del trapecio es: " + Figura.getArea());
+                }
+                break;
+
+                case 10:{
+                    Figura.areaCilindro(Double.parseDouble(JOptionPane.showInputDialog("Ingrese el radio del cilindro")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura del cilindro")) );
+
+                    Figura.volumenCilindro(Double.parseDouble(JOptionPane.showInputDialog("Ingrese el radio del cilindro")),
+                            Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura del cilindro")) );
+
+
+                    JOptionPane.showMessageDialog(null, "El área del cilindro es: " + Figura.getArea() +
+                            "\nEl volumen del cilindro es: " + Figura.getVolumen());
+
+                }
+                break;
+
+                case 0:{
+                    JOptionPane.showMessageDialog(null, "Desarrollado por Super compumundo hipermega red");
+                }
+                break;
+
+                default:{
+                    JOptionPane.showMessageDialog(null, "Opción no válida");
+                }
+                break;
+
+
+            }//fin del switch
+
+        }//FIN DEL WHILE PRINCIPAL
+
+
+
+    }//FIN DEL MÉTODO DE MAIN
+
+
+
+    public static int Menu(){
+        int opcion = 1;
+
+            opcion = Integer.parseInt( JOptionPane.showInputDialog("\tCalculadora de figuras geométricas\n" +
+                    "1) Área de un triángulo\n" +
+                    "2) Perímetro de un triángulo\n" +
+                    "3) Área y perímetro de un círculo\n" +
+                    "4) Área y volumen de una esfera\n" +
+                    "5) Área y perímetro de un cuadrado\n" +
+                    "6) Área y volumen de un cubo\n" +
+                    "7) Área y Perímetro de un rectángulo\n" +
+                    "8) Área y volumen de un paralelepipedo\n" +
+                    "9) Área de un trapecio\n" +
+                    "10) Área y volumen de un cilindro\n" +
+                    "0) SALIR" ) );
+
+        return opcion;
+    }//fin del método menu
+
+}
